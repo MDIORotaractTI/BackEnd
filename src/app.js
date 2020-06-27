73f7739093cd3952d8fd21ec42958cb5162b7cdb
Natalia Regina Lima Silva <natalia_regina10@outlook.com>
2020-06-27 12:42:29 -0300

    Ajustando configuracoes do heroku

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 const swaggerUI = require('swagger-ui-express');
 const swaggerDocument = require('./swagger');
 
-const port = 3000
+const port = process.env.PORT || 3000
 //porta usada
 
 //definição de rotas principais x classes js
